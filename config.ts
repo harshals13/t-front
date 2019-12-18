c1f3189bb7378b8583f93ceafc16f4cfd4adcdd7
Harsha <harsha@minebrat.com>
2019-12-19 00:28:39 +0530

    url change

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 export class AppConfig {
-  public static baseUrl = 'https://trush-p.herokuapp.com';
+  public static baseUrl = 'https://t-back.herokuapp.com/';
   public static baseUrlV1 =  AppConfig.baseUrl + '/api/v1';
 }
 
